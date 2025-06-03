e5891c979d20b126b902f0cd4f090983c979af7d
jam.jar <darkestknight3d@outlook.com>
2025-06-03 13:50:58 -0700

    started layout, created data structire

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+
+const projectCollection = defineCollection({
+    type: 'content',
+    schema: ({ image }) =>
+        z.object({
+            author: z.string(),
+            title: z.string(),
+            cover: image(),
+            summary: z.string(),
+        }),
+});
+
+export const collections = {
+    projects: projectCollection,
+};
